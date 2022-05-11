fe3ad5bc1c18a04f7cfc01b3fcd17b39c230a73d
aver343 <ankushvermagb@gmail.com>
2022-05-11 13:26:54 +0530

    changes

--- main.js
+++ main.js
@@ -50,8 +50,15 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
+app.on('ready', () => {
+  createWindow();
+   updateApp = require('update-electron-app');
+    updateApp({
+        repo: 'AVER343/photopea-desktop', // defaults to package.json
+        updateInterval: '1 hour',
+        notifyUser: true
+    });
+});
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
